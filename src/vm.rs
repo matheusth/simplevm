325f26cd906056cbe43b84a1da6e0192d5fda95c
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2024-08-06 11:07:42 -0300

    feat: add support for binary and hexdecimal numbers

--- src/vm.rs
+++ src/vm.rs
@@ -44,6 +44,27 @@ pub enum Op {
 fn parse_instruction_arg(ins: u16) -> u8 {
     (ins >> 8) as u8
 }
+
+impl Op {
+    fn encode_r1(r: Register) -> u16 {
+        (r as u16) & 0xf << 8
+    }
+    fn encode_r2(r: Register) -> u16 {
+        (r as u16) & 0xf << 12
+    }
+    fn parse_numeric(s: &str) -> Result<u8, String>{
+        if s.is_empty() {
+            return Err("Empity string".to_string());
+        }
+        let (num,radix) = match s {
+            s if s.starts_with('$') => (&s[1..], 16),
+            s if s.starts_with('%') => (&s[1..], 2),
+            _ => (s,10)
+        };
+        u8::from_str_radix(num, radix).map_err(|x| format!("{}", x))
+    }
+}
+
 impl TryFrom<u16> for Op {
     type Error = String;
     fn try_from(value: u16) -> Result<Self, Self::Error> {
@@ -59,7 +80,12 @@ impl TryFrom<u16> for Op {
                 Ok(Op::PopRegister(Register::try_from(arg)?))
             }
             3 => Ok(Op::AddStack),
-            4 => Ok(Op::Signal(parse_instruction_arg(value))),
+            4 => {
+                let reg1 = Register::try_from(((value & 0xf00) >> 8) as u8)?;
+                let reg2 = Register::try_from(((value & 0xf000) >> 12) as u8)?;
+                Ok(Op::AddRegister(reg1, reg2))
+            }
+            5 => Ok(Op::Signal(parse_instruction_arg(value))),
             _ => Err(format!("invalid instruction {:X}", value)),
         }
     }
@@ -72,11 +98,11 @@ impl TryFrom<String> for Op {
         let splited_value: Vec<&str> = value.split(' ').filter(|v| !v.is_empty()).collect();
         match splited_value[0] {
             "Push" => {
-                let arg: u16 = splited_value[1].parse().expect("Invalid argument");
+                let arg: u16 = Op::parse_numeric(splited_value[1])? as u16;
                 Ok(Op::Push(arg))
             }
             "PopRegister" => {
-                if let Ok(arg) = splited_value[1].parse::<u8>() {
+                if let Ok(arg) = Op::parse_numeric(splited_value[1]) {
                     return Ok(Op::PopRegister(arg.try_into().unwrap()));
                 }
                 Err(format!(
@@ -84,9 +110,15 @@ impl TryFrom<String> for Op {
                     splited_value[1]
                 ))
             }
+            "AddRegister" => {
+                if let (Ok(r1), Ok(r2)) = (splited_value[1].parse::<u8>(), splited_value[2].parse::<u8>()) {
+                    return Ok(Op::AddRegister(r1.try_into()?, r2.try_into()?));
+                }
+                Err(format!("AddRegister reviced invalid arguments: {:?}", splited_value))
+            },
             "AddStack" => Ok(Op::AddStack),
             "Signal" => {
-                let arg: u8 = splited_value[1].parse().expect("Invalid argument");
+                let arg: u8 = Op::parse_numeric(splited_value[1])?;
                 Ok(Op::Signal(arg))
             }
             _ => Err(format!("Invalid instruction {}", splited_value[0])),
@@ -95,14 +127,17 @@ impl TryFrom<String> for Op {
 }
 impl TryFrom<Op> for u16 {
     type Error = String;
-
     fn try_from(value: Op) -> Result<Self, Self::Error> {
         match value {
             Op::Push(x) => Ok(1 | x << 8),
             Op::PopRegister(x) => Ok(2 & 0xff | (x as u16) << 8),
             Op::AddStack => Ok(3),
-            Op::Signal(x) => Ok(4 & 0xff | (x as u16) << 8),
-            _ => Err("Unimplemented op!".to_string()),
+            Op::Signal(x) => Ok(5 & 0xff | (x as u16) << 8),
+            Op::AddRegister(r1, r2) => Ok(4 | Op::encode_r1(r1) | Op::encode_r2(r2)),
+            _ => Err(format!(
+                "Unimplemented op {}!",
+                u16::try_from(value).unwrap()
+            )),
         }
     }
 }
