d07cb576bf9b63b29f89a468293447f94396f294
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2024-08-06 05:04:08 -0300

    feat: implents traits required for the assembler

--- src/vm.rs
+++ src/vm.rs
@@ -44,8 +44,9 @@ pub enum Op {
 fn parse_instruction_arg(ins: u16) -> u8 {
     (ins >> 8) as u8
 }
-impl Op {
-    fn parse_instruction(value: u16) -> Result<Self, String> {
+impl TryFrom<u16> for Op {
+    type Error = String;
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
         let op = (value & 0xff) as u8;
         match op {
             0 => Ok(Op::Nop),
@@ -60,6 +61,43 @@ impl Op {
             3 => Ok(Op::AddStack),
             4 => Ok(Op::Signal(parse_instruction_arg(value))),
             _ => Err(format!("invalid instruction {:X}", value)),
+        }
+    }
+}
+
+impl TryFrom<String> for Op {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let splited_value: Vec<&str> = value.split(' ').filter(|v| !v.is_empty()).collect();
+        match splited_value[0] {
+            "Push" => {
+                let arg: u16 = splited_value[1].parse().expect("Invalid argument");
+                Ok(Op::Push(arg))
+            }
+            "PopRegister" => {
+                let arg: u8 = splited_value[1].parse().expect("Invalid argument");
+                Ok(Op::PopRegister(arg.into()))
+            }
+            "AddStack" => Ok(Op::AddStack),
+            "Signal" => {
+                let arg: u8 = splited_value[1].parse().expect("Invalid argument");
+                Ok(Op::Signal(arg))
+            }
+            _ => Err(format!("Invalid instruction {}", splited_value[0])),
+        }
+    }
+}
+impl TryFrom<Op> for u16 {
+    type Error = String;
+
+    fn try_from(value: Op) -> Result<Self, Self::Error> {
+        match value {
+            Op::Push(x) => Ok(1 | x << 8),
+            Op::PopRegister(x) => Ok(2 & 0xff | (x as u16) << 8),
+            Op::AddStack => Ok(3),
+            Op::Signal(x) => Ok(4 & 0xff | (x as u16) << 8),
+            _ => Err("Unimplemented op!".to_string()),
         }
     }
 }
@@ -105,13 +143,16 @@ impl Machine {
     }
     pub fn step(&mut self) -> Result<(), String> {
         let pc = self.registers[Register::PC as usize];
-        let instruction = self.memory.read_word(pc).ok_or(format!("failed to read memory on address {:X}.", pc))?;
+        let instruction = self
+            .memory
+            .read_word(pc)
+            .ok_or(format!("failed to read memory on address {:X}.", pc))?;
         self.registers[Register::PC as usize] = pc + 2;
         // instruction format = [0 0 0 0 0 0 0 0 | 0 0 0 0 0 0 0 0]
         //                          operator | arg(s)
         //                                   | 8 bit literal
         //                                   | REG1 | REG2
-        match Op::parse_instruction(instruction) {
+        match instruction.try_into() {
             Ok(Op::Nop) => Ok(()),
             Ok(Op::Push(v)) => self.push(v),
             Ok(Op::PopRegister(r)) => {
@@ -127,9 +168,7 @@ impl Machine {
                 self.registers[r1 as usize] += self.registers[r2 as usize];
                 Ok(())
             }
-            Ok(Op::Signal(signal)) => {
-                self.signal_handlers.get(&signal).unwrap()(self)
-            }
+            Ok(Op::Signal(signal)) => self.signal_handlers.get(&signal).unwrap()(self),
             Err(err) => Err(format!("{} on {}", err, pc)),
         }
     }
